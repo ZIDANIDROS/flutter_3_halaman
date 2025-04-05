d45ff35f986179a5c2283656e7d9404050da5efb
husein zidan <zidan.idros14@gmail.com>
2025-04-05 10:48:50 +0700

    build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.flutter_3_halaman"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "25.2.9519653"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
